17ee7b6e65c974b032ca0bd44ff4be436919fb4c
EternumDuck <omerhanyigitbaba@gmail.com>
2025-05-02 12:59:21 +0300

    Started week9

--- LabProject/obj/Debug/net8.0/LabProject.AssemblyInfo.cs
+++ LabProject/obj/Debug/net8.0/LabProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a1a7ce35642cdd723c995ddc36127e50dc4bb6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+011f36fd72bf4aed5171e44a68f121240846725b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
